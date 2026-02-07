6a67fb2e09b97ad00716bd0303678dda7f896cb2
kamogelo <kamozozobaete@gmail.com>
2026-02-07 10:04:37 +0200

    navbar updated

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+const navLinks=[
+    {href:"#about",label:"about" },
+    {href:"#projects",label:"project" },
+    {href:"#testimonials",label:"testimonials" },
+    {href:"#experience",label:"experience" },
+]
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header>
+        <nav>
+            <a>
+                PM<span>.</span>
+            </a>
+            {/*nav setup*/}
+            <div>
+                <div>
+                    {navLinks.map((link)=>{
+                        <a href={link.href} key={index}>{link.label}</a>
+                    })}
+                </div>
+            </div>
+        </nav>
+    </header>
   )
 }
 
